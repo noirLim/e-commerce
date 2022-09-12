4f16358ba4fa4f1d0a3c4cf45f3476b8972dba1b
Yulia <yulialim0220@gmail.com>
2022-09-12 09:26:51 +0700

    fist commit

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">Amazona</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
