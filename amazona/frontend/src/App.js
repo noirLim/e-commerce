aad9bccc1750658862432e3547e10a74a624bbc7
Yulia <yulialim0220@gmail.com>
2022-09-12 21:21:22 +0700

    adding-router

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,13 +1,23 @@
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <div>
       <header>
-       <a href="/">Amazona</a>
+        <Link to="/">Amazona</Link>
       </header>
       <main>
-        List Product
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen/>} />
+          <Route path="/" element={<HomeScreen/>} />
+        </Routes>
       </main>
     </div>
+    </BrowserRouter>
+   
   );
 }
 
