7b272afcbbfba2cd8803089ca131d4f827101a81
Yulia <yulialim0220@gmail.com>
2022-09-17 15:28:47 +0700

    rating-reviews

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,20 +1,32 @@
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
+import {Container, Navbar} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
       <header>
-        <Link to="/">Amazona</Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+            <Navbar.Brand>Amazona</Navbar.Brand>
+            </LinkContainer>       
+          </Container>
+        </Navbar>
+          
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen/>} />
           <Route path="/" element={<HomeScreen/>} />
         </Routes>
+        </Container>
       </main>
+      
     </div>
     </BrowserRouter>
    
--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,73 +1,63 @@
-import React, { useEffect, useReducer, useState } from 'react'
+import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom'
-import logger from 'use-reducer-logger'
+import logger from "use-reducer-logger";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
 
-const reducer = (state,action) =>{
-  switch(action.type){
-    case 'FETCH_REQUEST':
-      return{...state,loading:true}
-    case 'FETCH_SUCCESS':
-      return {...state, products:action.payload, loading:false}
-    case 'FETCH_FAIL':
-      return{...state,loading:false,error:action.payload}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
-}
+};
 
 const HomeScreen = () => {
   // const [products,setProducts] = useState([])
 
-  const [{loading,error,products}, dispatch] = useReducer(logger(reducer),{
-    products:[],
-    loading:true,
-    error:''
-  })
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
 
-  useEffect(()=>{
-      const fetchData = async() =>{
-        dispatch({type:'FETCH_REQUEST'})
-        try{
-          const result = await axios.get('/api/products')
-          dispatch({type:'FETCH_SUCCESS', payload:result.data})
-        }catch(err){
-          dispatch({type:'FETCH_FAIL', payload:err.message})
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
-      fetchData()
-  },[])
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {
-            loading ? <div>Loading</div>
-            : error ? <div>{error}</div>
-            : (
-              products.map((product) => (
-                <div className="product" key={product.slug}>
-                  <Link to={`/product/${product.slug}`}>
-                    <img src={product.image} alt={product.name} />
-                  </Link>
-                  <div className="product-info">
-                    <Link to={`/product/${product.slug}`}>
-                      <p>
-                        <strong>{product.name}</strong>
-                      </p>
-                    </Link>
-                    <p>
-                      <strong>${product.price}</strong>
-                    </p>
-                    <button>Add to cart</button>
-                  </div>
-                </div>
-              )
-            ))
-          }
-        </div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
